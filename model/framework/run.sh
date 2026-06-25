ee579b78204461719846a1d8e664b7f1418a71d1
Miquel Duran-Frigola <miquel@ersilia.io>
2026-06-25 13:41:42 +0200

    TEMP diagnostic v13: instrument run.sh (survives rewrite) to capture serve trace

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,1 +1,3 @@
-python $1/code/main.py $2 $3
+echo "RUNSH_START $(date +%H:%M:%S) pwd=$(pwd) a1=$1 a2=$2 a3=$3" >> /tmp/runsh.log 2>&1
+python $1/code/main.py $2 $3 2>> /tmp/runsh.log
+echo "RUNSH_END rc=$? $(date +%H:%M:%S)" >> /tmp/runsh.log 2>&1
